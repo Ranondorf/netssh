f637863582063e07854418aa24836084ced5b542
Rui <rui7@tuta.io>
2023-09-21 22:54:19 +1000

    Created test objects that generates output files

--- script_tester/tester.py
+++ script_tester/tester.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
 
 import os
+import subprocess
+
+
+class test():
+    def __init__(self, commands, devices):
+        self.commands = commands
+        self.devices = devices
+
 
 
 
@@ -17,21 +25,33 @@ def write_input_files(lines,target_file_name):
 
 # print(os.getcwd())
 
-tests = []
+
+def main():
 
 
+    test_object_list = []
 
+    test_object_list.append(test(["[linux]","date"],["[linux]","alias1","alias2","alias3"]))
+    test_object_list.append(test(["[linux]","date","<monkey>","ls"],["[linux]","alias1","<monkey>","alias2","alias3"]))
+    tester_dir = os.path.dirname(os.path.realpath(__file__))
+    commands_file = tester_dir + "/commands.txt" 
+    devices_file = tester_dir + "/devices.txt"
+    output_file = tester_dir + "/output.txt"
+    netssh_file = tester_dir[:-13] + "netssh.py"
 
-tester_dir = os.path.dirname(os.path.realpath(__file__))
-commands_file = tester_dir + "/commands.txt" 
-devices_file = tester_dir + "/devices.txt"
-netssh_file = tester_dir[:-13] + "netssh.py" 
-print(netssh_file)
+    for i in range(len(test_object_list)):
+        write_input_files(test_object_list[i].commands,commands_file)
+        write_input_files(test_object_list[i].devices,devices_file)
+        # Check if subprocess run can take in a list
+        run_program = subprocess.run([netssh_file,'-c',commands_file,'-d',devices_file,'-o',tester_dir + "/test_result/test_result%s.txt" % i])
+        print("The exit code was: %d" % run_program.returncode)
 
-
-write_input_files(["[linux]","date"],commands_file)
-
-
+if __name__ == "__main__":
+    try:
+        main()
+    except Exception as e:
+        print(str(e))
+        os._exit(1)
 
 
 
