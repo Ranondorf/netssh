2af5a8b6e4f7c8f2affb0d10a67da1c7b9aeb2ea
Rui <rui7@tuta.io>
2023-09-22 01:20:39 +1000

    Rudimentary output comparision in place

--- script_tester/tester.py
+++ script_tester/tester.py
@@ -2,7 +2,7 @@
 
 import os
 import subprocess
-
+import filecmp
 
 class test():
     def __init__(self, commands, devices):
@@ -20,6 +20,11 @@ def write_input_files(lines,target_file_name):
     target_file.close()
 
 
+
+def compare_text_files(results_file,expected_results_file):
+    return filecmp.cmp(results_file,expected_results_file,shallow=False)
+
+
 # Run this from your working directory
 # eg if netssh.py is in netssh/, so not that directory
 
@@ -30,9 +35,10 @@ def main():
 
 
     test_object_list = []
-
-    test_object_list.append(test(["[linux]","date"],["[linux]","alias1","alias2","alias3"]))
-    test_object_list.append(test(["[linux]","date","<monkey>","ls"],["[linux]","alias1","<monkey>","alias2","alias3"]))
+    # Basic test of a single device type
+    test_object_list.append(test(["[linux]","echo \"This is a test command\""],["[linux]","alias1","alias2","alias3"]))
+    # Single device type, introducing a grouping
+    test_object_list.append(test(["[linux]","echo \"This is a test command\"","<monkey>","echo \"This is another command\""],["[linux]","alias1","<monkey>","alias2","alias3"]))
     tester_dir = os.path.dirname(os.path.realpath(__file__))
     commands_file = tester_dir + "/commands.txt" 
     devices_file = tester_dir + "/devices.txt"
@@ -45,6 +51,13 @@ def main():
         # Check if subprocess run can take in a list
         run_program = subprocess.run([netssh_file,'-c',commands_file,'-d',devices_file,'-o',tester_dir + "/test_result/test_result%s.txt" % i])
         print("The exit code was: %d" % run_program.returncode)
+
+    for i in range(len(test_object_list)):
+        comp_result = compare_text_files(tester_dir + "/test_result/test_result%s.txt" % i,tester_dir + "/test_expected_result/test_result%s.txt" % i)
+        if comp_result:
+            print(f'Test {i}, passed')
+        else:
+            print(f'Test {i}, failed')
 
 if __name__ == "__main__":
     try:
